4ff977ba97207f6f113cd0fcb804befcb7a7cd7c
juanmf <juanmf@gmail.com>
2013-01-24 20:23:23 -0300

    Moving libs/* to / and adding 1st attempt of Composer.json to register this package with Packagist.org. Also added namespaces everywhere.

--- lib/RenderStep3/RenderPDF/tmp/README.txt
+++
@@ -1,3 +0,0 @@
-Note:
-This directory is used to place temporary files that are used as parameters to fop.
-They get deleted just befor returning the resulting PDF.
--- modules/export/actions/actions.class.php
+++ modules/export/actions/actions.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the reportPlugin package.
+ * This file is part of the NeatReports package.
  * (c) 2011-2012 Juan Manuel Fernandez <juanmf@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
